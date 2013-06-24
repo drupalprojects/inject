02a673350587edce9a4ae05e32e5b22f15815015
sylvainlecoy <sylvain.lecoy@gmail.com>
2013-06-24 15:55:46 +0200

    Cleaning inject.api.php in preparation for 2.0 stable release.

--- inject.api.php
+++ inject.api.php
@@ -15,59 +15,6 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
  */
 
 /**
- * @deprecated Configures the container after boot.
- *
- * This hook is triggered at the end of the boot process, just before hook_init
- * is about to start.
- *
- * Because this hook can be triggered in a bootstrap session, it is recommended
- * to dump the configuration on disk to avoid calling it on each page request.
- *
- * This hook will be removed in favor of hook_inject_build(ContainerBuilder) in
- * the stable 2.0 release.
- *
- * @see hook_inject_build().
- *
- * @param ContainerBuilder $container
- *   The container builder object.
- */
-function hook_inject_boot(ContainerBuilder $container) {
-  $container->setParameter('mailer.transport', 'sendmail');
-  $container->register('mailer', 'Mailer')
-    ->addArgument('%mailer.transport%');
-
-  $container->register('newsletter_manager', 'NewletterManager')
-    ->addArgument(new Reference('mailer'));
-}
-
-/**
- * @deprecated Configures the container after init.
- *
- * This hook is triggered at the end of the init process, it is recommended to
- * dump the configuration on disk to avoid calling it on each page request.
- *
- * This hook will be removed in favor of hook_inject_build(ContainerBuilder) in
- * the stable 2.0 release.
- *
- * @see hook_inject_build().
- *
- * @param ContainerBuilder $container
- *   The container builder object.
- */
-function hook_inject_init(ContainerBuilder $container) {
-  $loader = new XmlFileLoader(
-    $container,
-    new FileLocator(__DIR__.'/../config')
-  );
-
-  $loader->load('services.xml');
-
-  if ($config['advanced']) {
-    $loader->load('advanced.xml');
-  }
-}
-
-/**
  * Configures the container.
  *
  * It is only ever called once when the cache is empty.
